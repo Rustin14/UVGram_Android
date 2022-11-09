0fd0a62549521ac48821820b57f2720728a622ba
Gabriel <gabofl14@gmail.com>
2022-11-09 13:54:30 -0600

    Creación de actividad de Splash Screen y Pantalla de Inicio

---
+++ app/src/main/java/com/example/uvgram/PantallaInicio.java
@@ -0,0 +1,14 @@
+package com.example.uvgram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+    }
+}
